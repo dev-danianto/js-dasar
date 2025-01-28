826c4421e80fd94ce598873a99a17a38023e354c
Dani <dev.danianto@gmail.com>
2025-01-28 18:46:01 +0700

    Add files via upload

---
+++ Object.js
@@ -0,0 +1,11 @@
+//Apa itu Object?
+//Object adalah kumpulan data dan/atau fungsi yang memiliki nama.
+
+//Contoh Object
+let user = {
+  name: "John",
+  age: 30,
+};
+
+console.log(user.name); // Akan Print John
+console.log(user.age); // Akan Print 30
