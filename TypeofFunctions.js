21e970907f0fab20db6578531e5deab960299804
Dani <dev.danianto@gmail.com>
2025-01-28 18:45:29 +0700

    Add files via upload

---
+++ TypeofFunctions.js
@@ -0,0 +1,67 @@
+//Tipe - tipe Function
+
+//Function Declaration
+function sayHello() {
+  console.log("Hello World!");
+}
+
+sayHello(); // Akan Print Hello World!
+
+//Function Expression
+let sayHello2 = function () {
+  console.log("Hello World!");
+};
+
+sayHello2(); // Akan Print Hello World!
+
+//Arrow Function
+let sayHello3 = () => {
+  console.log("Hello World!");
+};
+
+sayHello3(); // Akan Print Hello World!
+
+//Arrow Function Dengan Return
+let sayHello4 = () => {
+  return "Hello World!";
+};
+
+console.log(sayHello4()); // Akan Print Hello World!
+
+//Apa perbedaan antara Function Declaration, Function Expression, dan Arrow Function?
+//Function Declaration adalah cara yang paling umum digunakan untuk membuat function.
+//Function Expression adalah cara lain untuk membuat function.
+//Arrow Function adalah cara lain untuk membuat function.
+
+//Function Declaration
+function sayHello5() {
+  console.log("Hello World!");
+}
+
+sayHello5(); // Akan Print Hello World!
+
+//Function Expression
+let sayHello6 = function () {
+  console.log("Hello World!");
+};
+
+sayHello6(); // Akan Print Hello World!
+
+//Arrow Function
+let sayHello7 = () => {
+  console.log("Hello World!");
+};
+
+sayHello7(); // Akan Print Hello World!
+
+//Apa fungsi dari Function Declaration, Function Expression, dan Arrow Function?
+
+//Function Declaration digunakan untuk membuat function yang dapat digunakan di seluruh kode.
+//Function Expression digunakan untuk membuat function yang hanya dapat digunakan setelah deklarasi.
+//Arrow Function digunakan untuk membuat function yang lebih singkat.
+
+//Apa guna Arrow pada Arrow Function?
+//Arrow digunakan untuk membuat function yang lebih singkat.
+
+//Kenapa bisa lebih singkat?
+//Karena Arrow Function tidak memerlukan keyword function dan return.
